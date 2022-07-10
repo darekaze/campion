f741299fdf9be59a0c6ae6cde8bf32c5f9c8184d
DaRwin <darekaze@gmail.com>
2022-07-10 11:46:36 +0800

    feat: player tab

--- src/modules/player/player-tab.vue
+++ src/modules/player/player-tab.vue
@@ -1,11 +1,41 @@
 <script setup lang="ts">
-import { IonToolbar, IonTitle } from '@ionic/vue'
+import { IonToolbar, IonLabel, IonIcon, IonThumbnail, IonButtons, IonButton } from '@ionic/vue'
+import { play, pause, playForward } from 'ionicons/icons'
+import { useAudioState } from '@/modules/audio'
 
+const player = useAudioState()
+
+const onPressed = () => {
+	player.playing ? player.pause() : player.play()
+}
+
+const onSkipPressed = () => {
+	player.skipTrack()
+}
 </script>
 
 <template>
-	<ion-toolbar class="h-20">
-		<!-- TODO: music player control -->
-		<ion-title>MUSIC PLAYER HERE</ion-title>
+	<ion-toolbar class="flex items-center">
+		<ion-thumbnail slot="start" class="thumb my-2 mr-2">
+			<img :src="player.currentTrack.artwork_url" />
+		</ion-thumbnail>
+		<ion-label>
+			<h4>{{ player.currentTrack.title }}</h4>
+			<p>{{ player.currentTrack.artist }}</p>
+		</ion-label>
+		<ion-buttons slot="end">
+			<ion-button color="dark" @click="onPressed">
+				<ion-icon slot="icon-only" :icon="player.playing ? pause : play" />
+			</ion-button>
+			<ion-button color="dark" @click="onSkipPressed">
+				<ion-icon slot="icon-only" :icon="playForward" />
+			</ion-button>
+		</ion-buttons>
 	</ion-toolbar>
 </template>
+
+<style scoped>
+.thumb {
+	--border-radius: 0.25rem;
+}
+</style>
