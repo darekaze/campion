ac6af437e6c0fd3fee76dc7615db1225626f729a
DaRwin <darekaze@gmail.com>
2022-07-12 19:43:15 +0800

    add: pwa plugins

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { VitePWA } from 'vite-plugin-pwa'
 import { resolve } from 'node:path'
 
 // https://vitejs.dev/config/
@@ -9,5 +10,5 @@ export default defineConfig({
 			'@': resolve(__dirname, './src'),
 		},
 	},
-	plugins: [vue()],
+	plugins: [vue(), VitePWA({ registerType: 'autoUpdate' })],
 })
