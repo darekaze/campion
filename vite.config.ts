95f06ed72341068739ffc56d1ecacc1193a132de
DaRwin <darekaze@gmail.com>
2022-07-13 02:49:08 +0800

    add: full pwa support

--- vite.config.ts
+++ vite.config.ts
@@ -10,5 +10,29 @@ export default defineConfig({
 			'@': resolve(__dirname, './src'),
 		},
 	},
-	plugins: [vue(), VitePWA({ registerType: 'autoUpdate' })],
+	plugins: [
+		vue(),
+		VitePWA({
+			registerType: 'autoUpdate',
+			includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'masked-icon.svg'],
+			manifest: {
+				name: 'Campion',
+				short_name: 'Campion',
+				description: 'Simple PWA Music App',
+				theme_color: '#1f1e33',
+				icons: [
+					{
+						src: 'pwa-192x192.png',
+						sizes: '192x192',
+						type: 'image/png',
+					},
+					{
+						src: 'pwa-512x512.png',
+						sizes: '512x512',
+						type: 'image/png',
+					},
+				],
+			},
+		}),
+	],
 })
