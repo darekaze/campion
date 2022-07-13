9d10023d2250376e977da07eac3820a42a567522
DaRwin <darekaze@gmail.com>
2022-07-14 01:20:47 +0800

    chore: move css variables

--- src/main.ts
+++ src/main.ts
@@ -13,9 +13,6 @@ import '@ionic/vue/css/core.css'
 import '@ionic/vue/css/normalize.css'
 import '@ionic/vue/css/structure.css'
 
-/* Theme variables */
-import './styles/variables.css'
-
 const pinia = createPinia()
 const app = createApp(App)
 	.use(IonicVue, {
