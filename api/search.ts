a5a88fcf29ef96c123470d2fa5eda814f33ec9dc
DaRwin <darekaze@gmail.com>
2022-07-15 05:19:42 +0800

    add: proxy to get search

---
+++ api/search.ts
@@ -0,0 +1,25 @@
+import type { VercelRequest, VercelResponse } from '@vercel/node'
+import axios from 'axios'
+
+export default async (req: VercelRequest, res: VercelResponse) => {
+	const { q = '', t = 't' } = req.query
+
+	const { data } = await axios.post(
+		'https://bandcamp.com/api/bcsearch_public_api/1/autocomplete_elastic',
+		{
+			search_text: q,
+			search_filter: t,
+			full_page: false,
+			fan_id: null,
+		},
+		{
+			headers: {
+				origin: 'https://bandcamp.com',
+				referer: 'https://bandcamp.com/',
+				'user-agent': 'Mozilla/5.0 (Windows NT 10.0; rv:102.0) Gecko/20100101 Firefox/102.0',
+			},
+		},
+	)
+
+	return res.status(200).json(data?.auto?.results ?? [])
+}
