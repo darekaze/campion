ef3f1c3fa4a5476bbb34b95ca553b4856530ad82
DaRwin <darekaze@gmail.com>
2022-07-22 00:30:20 +0800

    add: extract stream url

--- api/stream-url.ts
+++
@@ -1,7 +0,0 @@
-import type { VercelRequest, VercelResponse } from '@vercel/node'
-
-export default (req: VercelRequest, res: VercelResponse) => {
-	// TODO: impl. get stream url (extract from html file)
-	const { name } = req.query
-	res.status(200).send(`Hello ${name}!`)
-}
