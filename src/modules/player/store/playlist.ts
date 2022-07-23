bb371ef17cb0e7019e0e453bf5af2112a8babcd7
DaRwin <darekaze@gmail.com>
2022-07-24 05:11:45 +0800

    fix: first item in playlist

--- src/modules/player/store/playlist.ts
+++ src/modules/player/store/playlist.ts
@@ -2,7 +2,7 @@ import { getUnixTime } from 'date-fns'
 import { defineStore } from 'pinia'
 import { useStorage } from '@vueuse/core'
 import { fetchStreamUrl } from '@/utils/bandcamp'
-import { getSongs } from '@/utils/initial-data'
+import { usePlayerState } from './player'
 
 const CACHE_TTL = 3600
 
@@ -30,10 +30,10 @@ export const usePlaylistState = defineStore('playlist', {
 	state: () => {
 		const _streamUrlCaches = useStorage<StreamUrls>('streamurls', {})
 		const playlists = useStorage<Playlists>('playlists', {
-			favorite: getSongs(),
+			favorite: [],
 			recent: [],
 		})
-		const key = useStorage('activeKey', '')
+		const key = useStorage('activePlaylist', 'favorite')
 		const index = useStorage('currentIndex', 0)
 
 		return {
@@ -52,22 +52,31 @@ export const usePlaylistState = defineStore('playlist', {
 	},
 	actions: {
 		addTrackToPlaylist(name: string, track: Track) {
+			const player = usePlayerState()
 			const exist = this.playlists[name]?.some((i) => i.id === track.id)
 
-			if (!exist) {
-				this.playlists[name]?.push(track)
-				return true
-			}
+			if (exist) return false
 
-			return false
+			// Add item
+			this.playlists[name]?.push(track)
+
+			// Handle first track added
+			this.playlists[name]?.length === 1 && player.setTrack(name, 0)
+
+			return true
 		},
 		removeTrackFromPlaylist(name: string, index: number) {
+			const player = usePlayerState()
+
+			// Remove item
 			this.playlists[name]?.splice(index, 1)
 
 			// Adjust track if index is bigger than current index
 			this.index > index && (this.index -= 1)
 			// Reset index to 0 if index is final
 			this.index >= this.playlists[name]?.length && (this.index = 0)
+			// Handle last track deleted
+			!this.currentTrack && (player.playing = false)
 		},
 		async getStreamUrl(url: string) {
 			const unixTime = getUnixTime(new Date())
