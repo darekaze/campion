c4eedc2550a80647769f2754feb0d0183594ab19
DaRwin <darekaze@gmail.com>
2022-07-23 10:56:57 +0800

    fix: music player footer

--- src/modules/tab/container.vue
+++ src/modules/tab/container.vue
@@ -1,31 +1,37 @@
 <script setup lang="ts">
 import {
-	IonTabBar,
-	IonTabButton,
-	IonTabs,
-	IonLabel,
-	IonIcon,
 	IonPage,
 	IonRouterOutlet,
+	IonFooter,
+	IonTabs,
+	IonTabBar,
+	IonTabButton,
+	IonLabel,
+	IonIcon,
 } from '@ionic/vue'
-import { musicalNoteOutline, searchOutline, timeOutline } from 'ionicons/icons'
+import { musicalNoteOutline, searchOutline } from 'ionicons/icons'
+import PlayerTab from './player-tab.vue'
 </script>
 
 <template>
 	<ion-page>
 		<ion-tabs>
 			<ion-router-outlet></ion-router-outlet>
-			<ion-tab-bar slot="bottom">
-				<ion-tab-button tab="home" href="/t/home">
-					<ion-icon size="small" :icon="musicalNoteOutline" />
-					<ion-label>Playlist</ion-label>
-				</ion-tab-button>
+			<ion-footer slot="bottom">
+				<player-tab />
 
-				<ion-tab-button tab="search" href="/t/search">
-					<ion-icon size="small" :icon="searchOutline" />
-					<ion-label>Search</ion-label>
-				</ion-tab-button>
-			</ion-tab-bar>
+				<ion-tab-bar>
+					<ion-tab-button tab="home" href="/t/home">
+						<ion-icon size="small" :icon="musicalNoteOutline" />
+						<ion-label>Playlist</ion-label>
+					</ion-tab-button>
+
+					<ion-tab-button tab="search" href="/t/search">
+						<ion-icon size="small" :icon="searchOutline" />
+						<ion-label>Search</ion-label>
+					</ion-tab-button>
+				</ion-tab-bar>
+			</ion-footer>
 		</ion-tabs>
 	</ion-page>
 </template>
