69bae9bb6d78a54649e3be46078ddebbb44f8bb0
DaRwin <darekaze@gmail.com>
2022-07-24 12:47:44 +0800

    refactor: stream url regex

--- api/streamlink.ts
+++ api/streamlink.ts
@@ -1,9 +1,7 @@
 import got from 'got'
 import type { VercelRequest, VercelResponse } from '@vercel/node'
 
-const RE_STREAM_URL = /https:\/\/[^.]+\.bcbits\.com\/stream\/[^;"]*/g
-
-const htmlDecode = (str: string) => str.replace(/&amp;/g, '&').replace(/&quot;/g, '"')
+const RE_STREAM_URL = /https:\/\/[^.]+\.bcbits\.com\/stream\/.+?(?=&quot;)/g
 
 export default async (req: VercelRequest, res: VercelResponse) => {
 	const { url = '' } = req.query
@@ -12,10 +10,12 @@ export default async (req: VercelRequest, res: VercelResponse) => {
 
 	try {
 		const { body } = await got(url as string)
-		const [streamUrl] = htmlDecode(body).match(RE_STREAM_URL) ?? []
+		const [streamUrl] = body.match(RE_STREAM_URL) ?? []
 
-		return res.status(200).json({ url: streamUrl })
+		return streamUrl
+			? res.status(200).json({ url: streamUrl.replace(/&amp;/g, '&') })
+			: res.status(400).json({ error: 'Failed to get stream url' })
 	} catch {
-		return res.status(400).json({ error: 'url error' })
+		return res.status(400).json({ error: 'Endpoint or URL error' })
 	}
 }
