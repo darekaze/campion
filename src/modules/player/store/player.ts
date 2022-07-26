5313110f238f1b2469be22b471903477b7641c75
DaRwin <darekaze@gmail.com>
2022-07-26 11:56:06 +0800

    fix: first track loop to last track

--- src/modules/player/store/player.ts
+++ src/modules/player/store/player.ts
@@ -63,7 +63,11 @@ export const usePlayerState = defineStore('player', () => {
 	const skipTrack = (forward = true) => {
 		if (playlist.currentPlaylist?.length <= 1) return
 
-		playlist.index = (playlist.index + (forward ? 1 : -1)) % (playlist.currentPlaylist?.length ?? 1)
+		playlist.index =
+			playlist.index < 0
+				? playlist.currentPlaylist.length - 1
+				: (playlist.index + (forward ? 1 : -1)) % playlist.currentPlaylist.length
+
 		startAudio()
 	}
 
